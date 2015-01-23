7ce77f2a1450c76dab7fe8d147d828e535c018fa
Sandhya Diwana <diwanas@gmail.com>
2015-01-23 15:08:33 -0500

    Created resumeBuilder.js, updated helper.js, style.css
    Created bio, work, education, and projects object with encapsulated
    display functions in resumeBuilder.js.
    
    Updated style.css to remove indentation from 'Online Education' header.
    
    Took out flex-box class to display bio skills in a column and not row.

--- js/helper.js
+++ js/helper.js
@@ -23,37 +23,38 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="my picture">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+/* Sandhya - Took out flex-box class, so skills are not displayed in one row */
+var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a class="a-header" href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<a class="a-header" href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" alt="my project">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a class="a-header" href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineTitle = '<a class="a-header" href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a class="a-header" href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -65,7 +66,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName(name) || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -171,6 +172,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +235,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,495 @@
+
+
+/* --------------------------------------------------------------------------------
+
+Sandhya Diwana
+resumeBuilder.js
+
+The javaScript code reads from javaScript objects: bio, work, projects, and education.
+It then inserts them to html code with JQuery functions to displays them on the browser.
+The code also displays a map with locations from bio, work, and education, and
+captures the coordinates of mouse click on the web page and displays them on console
+
+-------------------------------------------------------------------------------- */
+
+
+/* BIO object */
+/* -------------------------------------------------------------------------------- */
+
+var bio = {
+
+				"name" : "Sandhya Diwana",
+
+				"role" : "Web Developer",
+
+				"contacts" : {"mobile" : "333-333-3333",
+
+								"email" : "diwanas@gmail.com",
+
+								"github" : "diwanas@gmail.com",
+
+							 	"twitter" : "diwanas",
+
+								"location" : "Columbia, MD 21046"
+
+			},
+
+			"welcomeMessage" : "Welcome to my resume",
+
+			"skills" : ["HTML", "CSS", "GIT", "JavaScript"],
+
+			"biopic" : "images/fry.jpg",
+
+			/* Encapsulate display function for bio */
+
+			"display" : function() {
+
+				/* declare temporary variables, replace %data% with bio object variable values, and preprend to HTML tag */
+
+				/* bio.role - since prepending, role first then name, but will get displayed as name then role */
+
+				var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+				$("#header").prepend(formattedRole);
+
+				/* bio.name */
+
+				var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+				$("#header").prepend(formattedName);
+
+				/* bio.contact.mobile */
+
+				var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+
+				$("#topContacts").prepend(formattedMobile);
+
+				/* bio.contact.email */
+
+				var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+
+				$("#topContacts").prepend(formattedEmail);
+
+				/* bio.contact.github */
+
+				var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+
+				$("#topContacts").prepend(formattedGitHub);
+
+				/* bio.contact.twitter */
+
+				var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+
+				$("#topContacts").prepend(formattedTwitter);
+
+				/* bio.contact.location */
+
+				var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+				$("#topContacts").prepend(formattedLocation);
+
+				/* bio.biopic */
+
+				var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+
+				$("#header").append(formattedBiopic);
+
+				/* bio.welcomeMessage */
+
+				var formattedWelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+				$("#header").append(formattedWelcomeMessage);
+
+				/* bio.skills */
+
+				var formattedSkill;
+
+				$("#header").append(HTMLskillsStart);
+
+				for (skill in bio.skills) {
+
+					formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+
+					$("#skills").append(formattedSkill);
+
+				}
+
+			}
+
+		};
+
+
+/* WORK object*/
+/* -------------------------------------------------------------------------------- */
+
+var work = {
+
+	"jobs" : [
+
+				{
+
+					"employer" : "Self",
+
+					"title" : "Consultant",
+
+					"location" : "1600 Pennsylvania Avenue Northwest, Washington, DC 20500",
+
+					"dates" : "2007 to Now",
+
+					"description" : "Web Developer and Whatever Web Developer and Whatever Web Developer and Whatever Web Developer and Whatever Web Developer and Whatever ",
+
+				},
+
+				{
+
+					"employer" : "Self",
+
+					"title" : "Consultant",
+
+					"location" : "Albuquerque, NM 87123",
+
+					"dates" : "2001 to 2007",
+
+					"description" : "Whatever Whatever Whatever Whatever Whatever Whatever Whatever Whatever Whatever Whatever Whatever Whatever",
+
+				}
+
+			],
+
+	/* Encapsulate display function for bio */
+
+	"display" : function() {
+
+				/* Insert WorkExperience Header */
+
+				$("#workExperience").append(HTMLworkStart);
+
+				/* declare temporary variables */
+
+				var formattedEmployer;
+
+				var formattedTitle;
+
+				var formattedLocation;
+
+				var formattedWorkDates;
+
+				var formattedWorkDescription;
+
+				/* Loop through jobs array, replace %data% with work object variable values, and preprend to HTML tag */
+
+				for (job in work.jobs) {
+
+					/* Format Employer and title, concat together for display */
+
+					formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+
+					formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+					$(".work-entry:last").append(formattedEmployer + formattedTitle);
+
+					/* Work Location */
+
+					formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+
+					$(".work-entry:last").append(formattedLocation);
+
+					/* Work Dates */
+
+					formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+
+					$(".work-entry:last").append(formattedWorkDates);
+
+					/* Work Description */
+
+					formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+					$(".work-entry:last").append(formattedWorkDescription);
+
+				}
+
+			}
+
+		};
+
+
+/* PROJECTS object */
+/* -------------------------------------------------------------------------------- */
+
+var projects = {
+
+		"projects" : [{
+
+					"title" : "HTML website",
+
+					"dates" : "December 2014",
+
+					"description" : "Use all HTML tags and formatting",
+
+					"images" : ["images/project.png", "images/project.png"]
+
+				},
+
+				{
+
+					"title" : "CSS and HTML website",
+
+					"dates" : "December 2014",
+
+					"description" : "Use CSS file to format HTML file",
+
+					"images" : []
+
+			},
+
+			{
+
+				"title" : "JavaScript website",
+
+				"dates" : "January 2015",
+
+				"description" : "Incorporate JavaScript along with HTML and CSS",
+
+				"images" : []
+
+			}],
+
+		"display" : function() {
+
+				/* Display Project Header */
+
+				$("#projects").append(HTMLprojectStart);
+
+				/* Declare temporary variables */
+
+				var formattedTitle;
+
+				var formattedDates;
+
+				var formattedDescription;
+
+				/* Loop through projects, format, and append them to HTML tags */
+
+				for (project in projects.projects) {
+
+					/* Project Titles */
+
+					formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+
+					$(".project-entry:last").append(formattedTitle);
+
+					/* Project Dates */
+
+					formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+
+					$(".project-entry:last").append(formattedDates);
+
+					/* Project Descriptions */
+
+					formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+
+					$(".project-entry:last").append(formattedDescription);
+
+					/* Declare temporary variable, loop through Image arrray and format and display Project Images */
+
+					var formattedImage;
+
+					if (projects.projects[project].images.length > 0)
+
+					{
+
+						for (image in projects.projects[project].images)
+
+						{
+
+							formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+
+							$(".project-entry:last").append(formattedImage);
+
+						}
+
+					}
+
+				}
+
+			}
+
+		};
+
+
+/* EDUCATION object */
+/* -------------------------------------------------------------------------------- */
+
+var education = {
+
+	"schools" : [{
+
+				"name" : "Johns Hopkins University",
+
+				"degree" : "Master Degree",
+
+				"dates" : 1996,
+
+				"location" : "Baltimore, MD 21218-2683",
+
+				"major" : "Computer Science"
+
+			},
+
+			{
+
+				"name" : "University of Maryland",
+
+				"degree" : "Bachelor Degree",
+
+				"dates" : 1989,
+
+				"location" : "College Park, MD 20742",
+
+				"major" : "Computer Science"
+
+			}],
+
+	"onlineCourses" : [{
+
+				"title" : "HTML",
+
+				"school" : "Udacitty",
+
+				"date" : 2014,
+
+				"url" : "http://udacity.com"
+
+			},
+
+			{
+
+				"title" : "JavaScript",
+
+				"school" : "Udacitty",
+
+				"date" : 2015,
+
+				"url" : "http://udacity.com"
+
+			}],
+
+	"display" : function() {
+
+				/* Append Education header */
+
+				$("#education").append(HTMLschoolStart);
+
+				/* Declare temporary variables */
+
+				var formattedSchoolName;
+
+				var formattedSchoolDegree;
+
+				var formattedSchoolDates;
+
+				var formattedSchoolLocation;
+
+				var formattedSchoolMajor;
+
+				/* Loop through schools, format and append the variables to HTML tags */
+
+				for (school in education.schools) {
+
+					/* Education school name and degree */
+
+					formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+
+					formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+
+					$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+
+					/* School dates */
+
+					formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+
+					$(".education-entry:last").append(formattedSchoolDates);
+
+					/* School location */
+
+					formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+
+					$(".education-entry:last").append(formattedSchoolLocation);
+
+					/* School major */
+
+					formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+
+					$(".education-entry:last").append(formattedSchoolMajor);
+
+				}
+
+				/* Display Online Class header */
+
+				$(".education-entry").append(HTMLonlineClasses);
+
+				/* Declare temporary variables */
+
+				var formattedOnlineTitle;
+
+				var formattedOnlineSchool;
+
+				var formattedOnlineDate;
+
+				var formattedOnlineURL;
+
+				/* Loop through online courses, format and append them to HTML tags */
+
+				for (course in education.onlineCourses) {
+
+					/* format Title and School name, concat and append them to HTML tag */
+
+					formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+
+					formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+
+					$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+
+					/* Online course dates */
+
+					formattedOnlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
+
+					$(".education-entry:last").append(formattedOnlineDate);
+
+					/* Online school URLs */
+
+					formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+
+					$(".education-entry:last").append(formattedOnlineURL);
+
+				}
+
+		}
+
+};
+
+
+/* MAP */
+/* -------------------------------------------------------------------------------- */
+/* Map is displayed through helper.js */
+
+/* Append a google map */
+$("#mapDiv").append(googleMap);
+
+
+/* OBJECT FUNCTION CALLS */
+/* -------------------------------------------------------------------------------- */
+/* Call functions to display resume bio, education, work, and project sections */
+
+
+bio.display();
+
+education.display();
+
+work.display();
+
+projects.display();
+
+
+
+
